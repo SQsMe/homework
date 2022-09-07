e24d0f58bffd5ffca0bfdad70d7779e851fc2308
SQsMe <goldswag1996@mail.ru>
2022-09-07 23:05:38 +0300

    one

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿Int num = Convert.ToInt32(Console.ReadLine("Введите число"):
+int run = Convert.ToInt32(Console.ReadLine())
+
+    int result = num * num;
+    System.Console.WriteLine($"Квадрат числа{num} -> {result}")
+
+
